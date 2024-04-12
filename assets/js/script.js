2111d9ffbfd51af7ef92680145ec4eaa44780730
Michal Slodowy <mix1999a@gmail.com>
2024-04-12 16:41:38 +0200

    update 0.1

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+    var projects = [
+        {
+            "title": "Customer Request Service System",
+            "technologies": "Spring Boot, React, MySQL, Docker",
+            "description": "The thesis outlines a secure customer inquiry app for online stores, using Spring Boot, React, and Docker, a project I independently developed. It prioritizes security with Spring Security 6 and emphasizes modern tech for efficient customer service.",
+            "img_src": "images/support.png"
+        },
+        {
+            "title": "Audio Player",
+            "technologies": ".NET, C#, Maui",
+            "description": "In Maui, I spearheaded the development as the lead programmer responsible for the core functionality of a user-friendly audio player app. It enables seamless organization, browsing, and playback of music tracks with advanced features for playlist creation and customization, offering a smooth music listening experience.",
+            "img_src": "images/musicplayer.jpeg"
+        },
+        {
+            "title": "30 Seconds!",
+            "technologies": "Java, PHP, MySQL, SQLite",
+            "description": "The '30 Seconds!' app on Android, a project I single-handedly developed, helps users learn languages by offering tasks in Polish, English, German, Italian, and Spanish, with a user-friendly interface.",
+            "img_src": "images/30sec.png"
+        },
+        {
+            "title": "Yourtube",
+            "technologies": "PHP, PostgreSql, JavaScript, jQuery",
+            "description": "Developed YourTube, a dynamic video streaming platform, where I served as the lead backend programmer responsible for ensuring smooth video playback and easy video uploads. Utilized PHP, JavaScript, and jQuery to create a seamless user experience, showcasing proficiency in both front-end and back-end development for a user-friendly interface.",
+            "img_src": "images/yourtube.png"
+        }
+    ];
+
+ 
+
+
